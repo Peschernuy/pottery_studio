ed9f3188fcff30a80ae606cab7bc40804eed3fbc
phalmer1 <f.phalmer@gmail.com>
2024-01-27 16:17:34 +0100

    fix-errors-and-create-assets/images/header

--- src/components/Header/Navbar/Navbar.jsx
+++ src/components/Header/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Navbar.scss';
+import './navbar.scss';
 import { Link, useLocation } from 'react-router-dom';
 
 const Navbar = () => {
@@ -15,23 +15,23 @@ const Navbar = () => {
             </div>
             <div>
                 <Link to="/">ABOUT</Link>
-                <span className={location.pathname === '' && 'w-full'}></span>
+                {/* <span className={location.pathname === '/about' && 'w-full'}></span> */}
             </div>
             <div>
                 <Link to="/">SHOP</Link>
-                <span className={location.pathname === '' && 'w-full'}></span>
+                {/* <span className={location.pathname === '/' && 'w-full'}></span> */}
             </div>
             <div>
                 <Link to="/">BOOK CLASSES</Link>
-                <span className={location.pathname === '' && 'w-full'}></span>
+                {/* <span className={location.pathname === 's' && 'w-full'}></span> */}
             </div>
             <div>
                 <Link to="/">OPEN HOURS</Link>
-                <span className={location.pathname === '' && 'w-full'}></span>
+                {/* <span className={location.pathname === 's' && 'w-full'}></span> */}
             </div>
             <div>
                 <Link to="/">CONTACTS</Link>
-                <span className={location.pathname === '' && 'w-full'}></span>
+                {/* <span className={location.pathname === 's' && 'w-full'}></span> */}
             </div>
         </nav>
     );
