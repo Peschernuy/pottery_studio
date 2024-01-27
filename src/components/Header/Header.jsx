a97028707aed0cf70c91349fc6fb5db6ad05ffc7
phalmer1 <f.phalmer@gmail.com>
2024-01-27 15:44:57 +0100

    add header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,29 @@
-import './header.scss'
-
+import './header.scss';
+import logoHeader from './logo_header.svg';
+import btnlogin from './btn_login.svg';
+import btnSearch from './btn_search.svg';
+import btnBascet from './btn_basket.svg';
+import Navbar from './Navbar/Navbar';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+    return (
+        <header>
+            <div className="header">
+                <Link to={'/'} className="header__logo">
+                    <img src={logoHeader} alt="" />
+                </Link>
+                <Navbar />
+                <div className="header__btn">
+                    <img src={btnSearch} alt="" />
+                    <img src={btnlogin} alt="" />
+                    <Link to="/cart">
+                        <img src={btnBascet} alt="" />
+                    </Link>
+                </div>
+            </div>
+        </header>
+    );
+};
 
-export default Header
+export default Header;
