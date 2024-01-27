5f1afd03831216f334ae93f4b47c7f301ffa0ba7
phalmer1 <f.phalmer@gmail.com>
2024-01-27 16:23:22 +0100

    add-footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,32 @@
-import './footer.scss'
-
-
+import './footer.scss';
+import logo from '../../assets/images/footer/logo_footer.svg';
+import btn_instagram from '../../assets/images/footer/btn_instagram.svg';
+import btn_facebook from '../../assets/images/footer/btn_facebook.svg';
+import btn_telegram from '../../assets/images/footer/btn_telegram.svg';
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+    return (
+        <footer className="footer ">
+            <div className="footer__content">
+                <div className="footer__nav__hrefs">
+                    <div>About</div>
+                    <div>Shop</div>
+                    <div>Book classes</div>
+                    <div>Open hours</div>
+                    <div>Contacts</div>
+                </div>
+                <div className="footer__logo">
+                    <img src={logo} alt="" />
+                    <div>2024 © All rights reserved</div>
+                </div>
+                <div className="footer__contact__icons">
+                    <img src={btn_instagram} alt="" />
+                    <img src={btn_facebook} alt="" />
+                    <img src={btn_telegram} alt="" />
+                </div>
+            </div>
+        </footer>
+    );
+};
 
-export default Footer
+export default Footer;
