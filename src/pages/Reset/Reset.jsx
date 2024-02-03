d0237829f29e6306e2414267a5fd6b3697eae092
Mykhailo Koshmak <peschernuy@gmail.com>
2024-02-03 16:33:46 +0200

    add reset page

--- src/pages/Reset/Reset.jsx
+++ src/pages/Reset/Reset.jsx
@@ -1,11 +1,20 @@
-import './reset.scss'
-
-
+import "./reset.scss";
+import { Form, Link } from "react-router-dom";
+import { FormInput, SubmitBtn } from "../../components";
 
 const Reset = () => {
   return (
-    <div>Reset</div>
-  )
-}
+    <section className="reset__container">
+      <Form className="reset__form" method="post">
+        <h4 className="reset__title">Reset password</h4>
+        <p className="reset__subtitle">
+          We will send you an email to reset your password
+        </p>
+        <FormInput type="email" name="email" placeholder="email *" />
+        <SubmitBtn className="reset__submit--btn" text="Sumbit" />
+      </Form>
+    </section>
+  );
+};
 
-export default Reset
+export default Reset;
